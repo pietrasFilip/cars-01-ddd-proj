feeba14b8b8459adc857e177d9a8831adfdaa00b
Filip <pietrasfilip2@gmail.com>
2023-12-23 18:16:37 +0100

    Added code to run spark dev server.

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,7 +1,32 @@
 package com.app;
 
+import com.app.infrastructure.api.routing.CarsRouting;
+import com.app.infrastructure.api.routing.ErrorRouting;
+import com.app.infrastructure.api.routing.SecurityRouting;
+import com.app.infrastructure.api.routing.UserRouting;
+import com.app.infrastructure.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static spark.Spark.initExceptionHandler;
+import static spark.Spark.port;
+
 public class Main {
     public static void main(String[] args) {
+        initExceptionHandler(e -> System.out.println(e.getMessage()));
+        port(8080);
 
+        var context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        var errorRouting = context.getBean("errorRouting", ErrorRouting.class);
+        errorRouting.routes();
+
+        var securityRouting = context.getBean("securityRouting", SecurityRouting.class);
+        securityRouting.routes();
+
+        var carsRouting = context.getBean("carsRouting", CarsRouting.class);
+        carsRouting.routes();
+
+        var userRouting = context.getBean("userRouting", UserRouting.class);
+        userRouting.routes();
     }
 }
